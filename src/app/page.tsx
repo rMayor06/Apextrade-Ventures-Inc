d314415d9044559f89d22018a63d12b707a91418
rMayor06 <165641747+rMayor06@users.noreply.github.com>
2024-04-05 14:50:19 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,16 @@ import EmpList from "./components/empList";
 export default async function Home() {
   return (
     <>
-      <div>
-        <h1>HOME</h1>
+      <div className="bg-blue-300 mb-7">
+        <img
+          src="/favicon.ico"
+          width={500}
+          height={500}
+          className="stroke-cyan-500 mx-auto mt-7"
+        />
+        <h1 className="text-3xl font-bold whitespace-nowrap dark:text-black text-center">
+          APEXTRADE VENTURES INCORPORATION
+        </h1>
       </div>
     </>
   );
